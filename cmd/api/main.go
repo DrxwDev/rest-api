2a51fd075247b4cad4c25f96d16d5f7b3ce154bc
DrxwDev <drxwdev@outlook.es>
2026-07-04 01:37:17 -0400

    fix: add render env

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,18 +2,23 @@ package main
 
 import (
 	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.uber.org/fx"
 
 	"github.com/DrxwDev/rest-api/internal/config"
 	"github.com/DrxwDev/rest-api/internal/database"
 	"github.com/DrxwDev/rest-api/internal/logger"
 	"github.com/DrxwDev/rest-api/internal/server"
-	"github.com/joho/godotenv"
-	"go.uber.org/fx"
 )
 
 func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("error loading .env file")
+	if os.Getenv("RENDER") == "" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Println("No .env file found")
+		}
 	}
 
 	fx.New(
